735f3facdc3b2b41f4a3894fcd546937f8553cf0
JDDev0 <jddev0@dorneger.at>
2026-06-13 15:06:22 +0200

    Add recipe viewer integration for RU Farmland recipes

---
+++ src/main/java/me/jddev0/epru/integration/emi/EnergizedPowerRUEMIPlugin.java
@@ -0,0 +1,66 @@
+package me.jddev0.epru.integration.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiCraftingRecipe;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import me.jddev0.epru.recipe.PeatFarmlandCraftingRecipe;
+import me.jddev0.epru.recipe.SiltFarmlandCraftingRecipe;
+import net.minecraft.core.NonNullList;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.regions_unexplored.block.RuBlocks;
+
+import java.util.Optional;
+
+@EmiEntrypoint
+public class EnergizedPowerRUEMIPlugin implements EmiPlugin {
+
+    @Override
+    public void register(EmiRegistry registry) {
+        registerRecipes(registry);
+    }
+
+    private void registerRecipes(EmiRegistry registry) {
+        RecipeManager recipeManager = registry.getRecipeManager();
+
+        //Add aether farmland special crafting recipe if loaded
+        {
+            Optional<RecipeHolder<CraftingRecipe>> recipeOptional = recipeManager.getAllRecipesFor(RecipeType.CRAFTING).stream().
+                    filter(recipe -> recipe.value() instanceof PeatFarmlandCraftingRecipe).findFirst();
+            if(recipeOptional.isPresent()) {
+                ShapelessRecipe recipe = new ShapelessRecipe("", CraftingBookCategory.MISC, new ItemStack(RuBlocks.PEAT_FARMLAND.get()),
+                        NonNullList.of(null, new Ingredient[] {
+                                Ingredient.of(ItemTags.HOES),
+                                Ingredient.of(RuBlocks.PEAT_DIRT.get())
+                        }));
+
+                registry.addRecipe(new EmiCraftingRecipe(
+                        recipe.getIngredients().stream().map(EmiIngredient::of).toList(),
+                        EmiStack.of(recipe.getResultItem(null)),
+                        recipeOptional.get().id()
+                ));
+            }
+        }
+        {
+            Optional<RecipeHolder<CraftingRecipe>> recipeOptional = recipeManager.getAllRecipesFor(RecipeType.CRAFTING).stream().
+                    filter(recipe -> recipe.value() instanceof SiltFarmlandCraftingRecipe).findFirst();
+            if(recipeOptional.isPresent()) {
+                ShapelessRecipe recipe = new ShapelessRecipe("", CraftingBookCategory.MISC, new ItemStack(RuBlocks.SILT_FARMLAND.get()),
+                        NonNullList.of(null, new Ingredient[] {
+                                Ingredient.of(ItemTags.HOES),
+                                Ingredient.of(RuBlocks.SILT_DIRT.get())
+                        }));
+
+                registry.addRecipe(new EmiCraftingRecipe(
+                        recipe.getIngredients().stream().map(EmiIngredient::of).toList(),
+                        EmiStack.of(recipe.getResultItem(null)),
+                        recipeOptional.get().id()
+                ));
+            }
+        }
+    }
+}
